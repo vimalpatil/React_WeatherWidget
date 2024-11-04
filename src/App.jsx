7040dcb3f75c338ef965697cc3ec9c361fdb1cc1
Vimal Patil <vimalpatil287@gmail.com>
2024-11-04 13:22:38 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
+function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
